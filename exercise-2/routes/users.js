8744a2991a2054de95b63b8ec382932b199afff7
arusafa <arusafacollege@gmail.com>
2023-03-19 23:46:51 -0400

    lab06

---
+++ exercise-2/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+    }
+);
+
+router.post('/users', function(req, res, next) {
+    console.log('First Name: ' + req.body.firstname);
+    console.log('Last Name: ' + req.body.lastname);
+    res.send('POST received');
+    }
+);
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = router;
